289d359e274bd97e42dcf9409e9cf585da709a38
Kunga Tashi <kuntashtashi11@gmail.com>
2024-02-27 03:11:28 +0530

    Shadcn init and pages setup

---
+++ src/app/new-task/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function NewTaskPage() {
+  return (
+    <main className="">
+      {/* new task page */}
+      new task page
+    </main>
+  );
+}
